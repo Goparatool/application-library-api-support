782727876d9b837cbe2b3f90fca3349df7d1cf6e
sophiawang2008 <sophia_wangmi@hotmail.com>
2015-11-18 09:21:25 -0800

    Update ab-login-perf-test.bat

--- application-library-client/src/test/java/com/paratool/applib/client/perftest/ab-login-perf-test.bat
+++ application-library-client/src/test/java/com/paratool/applib/client/perftest/ab-login-perf-test.bat
@@ -1,1 +1,1 @@
-c:\Apache\bin\ab.exe -p login-post.json -T application/json  -c 10 -n 1000 https://xxx/uoapi/auth/email-login
+c:\Apache\bin\ab.exe -p login-post.json -T application/json  -c 10 -n 1000 https://applib.goparatoolapi.com/uoapi/auth/email-login
