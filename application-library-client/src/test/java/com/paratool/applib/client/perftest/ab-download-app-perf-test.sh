ce8b484620afb4734b52e81e7ce131f789195d72
sophiawang2008 <sophia_wangmi@hotmail.com>
2015-11-18 09:22:13 -0800

    Update ab-download-app-perf-test.sh

--- application-library-client/src/test/java/com/paratool/applib/client/perftest/ab-download-app-perf-test.sh
+++ application-library-client/src/test/java/com/paratool/applib/client/perftest/ab-download-app-perf-test.sh
@@ -1,1 +1,1 @@
-ab -p download-app.json -H "access_token: 14d67cc9-115b-36db-a711-bda03fe4fcab" -T application/json  -c 10 -n 1000 http://localhost:8080/uoapi/app/download
+ab -p download-app.json -H "access_token: 14d67cc9-115b-36db-a711-bda03fe4fcab" -T application/json  -c 10 -n 1000 https://applib.goparatoolapi.com/uoapi/app/download
