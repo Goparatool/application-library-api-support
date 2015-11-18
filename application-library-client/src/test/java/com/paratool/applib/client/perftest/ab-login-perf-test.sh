3da90f74f74ddc6569a614114725c5372bd57459
sophiawang2008 <sophia_wangmi@hotmail.com>
2015-11-18 09:29:57 -0800

    Update ab-login-perf-test.sh

--- application-library-client/src/test/java/com/paratool/applib/client/perftest/ab-login-perf-test.sh
+++ application-library-client/src/test/java/com/paratool/applib/client/perftest/ab-login-perf-test.sh
@@ -1,1 +1,1 @@
-ab -p login-post.json -T application/json  -c 10 -n 1000 http://localhost:8080/uoapi/auth/email-login
+ab -p login-post.json -T application/json  -c 10 -n 1000 https://applib.goparatoolapi.com/uoapi/auth/email-login
